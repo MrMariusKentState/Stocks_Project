32ebd48bde333fe1bc515800b6ae36fb03e6bb53
Marius Paulikas <mariuspaulikas@yahoo.com>
2022-05-18 16:39:55 -0400

    Banking Service updates

--- src/main/java/com/MariusPaulikas/Services/BankingService.java
+++ src/main/java/com/MariusPaulikas/Services/BankingService.java
@@ -1,5 +1,32 @@
 package com.MariusPaulikas.Services;
 
-public class BankingService {
+import java.util.List;
+import java.util.Optional;
 
+import com.MariusPaulikas.Models.Banking;
+import com.MariusPaulikas.Repositories.BankingRepository;
+
+public class BankingService { 
+	
+	private final BankingRepository bankingrepository;
+	
+	public BankingService(BankingRepository bankingrepository) {
+		this.bankingrepository = bankingrepository;
+	}
+	
+	public List<Banking> allAccounts() {
+		return bankingrepository.findAll();
+	}
+	
+	
+	public Banking findAccounts(Long id) {
+		Optional<Banking> oneaccount = bankingrepository.findById(id);
+		if(oneaccount.isPresent()) {
+			return oneaccount.get();
+		}
+		else {
+			return null;
+		}
+	
+	}
 }
