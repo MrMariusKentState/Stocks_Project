d9d4afa2667f7f62c80dea660be79c1cfdae319c
Marius Paulikas <mariuspaulikas@yahoo.com>
2022-05-18 16:47:58 -0400

    brokerage service updates

--- src/main/java/com/MariusPaulikas/Services/BrokerageService.java
+++ src/main/java/com/MariusPaulikas/Services/BrokerageService.java
@@ -1,5 +1,35 @@
 package com.MariusPaulikas.Services;
 
-public class BrokerageService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
+import com.MariusPaulikas.Models.Brokerage;
+import com.MariusPaulikas.Repositories.BrokerageRepository;
+
+@Service
+public class BrokerageService {
+	
+	private final BrokerageRepository brokeragerepository;
+
+	public BrokerageService (BrokerageRepository brokeragerepository) {
+		this.brokeragerepository = brokeragerepository;
+	}
+	
+	public List<Brokerage> allBrokerageAccounts () {
+		return brokeragerepository.findAll();
+	}
+	
+	
+	public Brokerage findBrokerageAccounts (Long id) {
+		Optional<Brokerage> onebrokerageaccount = brokeragerepository.findById(id);
+		if(onebrokerageaccount.isPresent()) {
+			return onebrokerageaccount.get();
+		}
+		else {
+			return null;
+		}
+	}
+	
 }
